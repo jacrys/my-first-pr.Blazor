d635870ad6be74bf8b121227201a9cda7d68d27b
Jacrys <kclewis0614@gmail.com>
2021-09-17 08:49:02 -0500

    start fresh
    1st working .NET 6 copy
    
    First fully working build
    
    Add container to prevent horizontal scroll
    
    Remove Lang Icons and unneeded code
    
    Add PH Badge
    
    Redid color scheme and made site nicer
    
    make footer fixed
    
    add fallback for config to env-vars
    
    Make config file optional
    
    Add debugging
    
    troubleshoot debug
    
    continuing troubleshooting
    
    still troubleshooting
    
    make redirect dynamic
    
    I've been working on troubleshooting....
    
    Please be almost done....
    
    try changing cookie mode
    
    test

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿namespace my_first_pr.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AuthController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            _logger.LogError(returnUrl);
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,25 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace my_first_pr.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        public IReadOnlyList<Issue> Issues { get; set; }
+        public Octokit.Language Langs { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public static readonly SearchIssuesRequest HacktoberfestRequest = new SearchIssuesRequest() {
+            State = ItemState.Open,
+            Labels = new List<string>() {
+                "hacktoberfest"
+            }
+        };
+
+        public static readonly SearchIssuesRequest HacktoberfestFirstRequest = new SearchIssuesRequest() {
+            State = ItemState.Open,
+            Labels = new List<string>() {
+                "hacktoberfest",
+                "good-first-issue"
+            }
+        };
+
+        public static readonly SearchIssuesRequest HacktoberfestEasyRequest = new SearchIssuesRequest() {
+            State = ItemState.Open,
+            Labels = new List<string>() {
+                "hacktoberfest",
+                "difficulty:+easy"
+            }
+        };
+
+        public async Task OnGetAsyc() {}
+
+        public async Task OnPostAsync(string language,string search)
         {
-            _logger = logger;
+            if (User.Identity is not null && User.Identity.IsAuthenticated)
+            {
+                string accessToken = await HttpContext.GetTokenAsync("access_token");
+                var github = new GitHubClient(new ProductHeaderValue("AspNetCoreGitHubAuth"), new InMemoryCredentialStore(new Credentials(accessToken)));
+                var currentSearch = GetSearchIssuesRequest(search);
+                if (!String.IsNullOrEmpty(language))
+                {
+                    currentSearch.Language = (Octokit.Language)(Int32.Parse(language));
+                }
+                else
+                {
+                    currentSearch.Language = null;
+                }
+                Issues = (await github.Search.SearchIssues(currentSearch)).Items;
+                currentSearch = null;
+            }
         }
 
-        public void OnGet()
-        {
-
-        }
+        private SearchIssuesRequest GetSearchIssuesRequest(string search)
+		{
+            switch (search)
+			{
+                case "HACKTOBERFEST_ISSUES":
+                    return HacktoberfestRequest;
+                case "HACKTOBERFEST_EASY_ISSUES":
+                    return HacktoberfestEasyRequest;
+                case "HACKTOBERFEST_FIRST_ISSUES":
+                    return HacktoberfestFirstRequest;
+                default:
+                    return null;
+            }
+		}
     }
+
 }
